d143ffed2abe7c88cb22f79f1d9c5b4fe8e2ced1
nileshjd2202 <nileshdangi2016@gmail.com>
2025-02-09 13:48:34 +0530

    flask todo app done

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
@@ -35,7 +35,7 @@ def update(sno):
     if request.method == "POST":
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo.query.filter_by(sno=sno).first()
+        todo = Todo.query.get(sno)
         todo.title = title
         todo.desc = desc
         db.session.add(todo)
@@ -43,13 +43,13 @@ def update(sno):
 
         return redirect("/")
     
-    todo = todo = Todo.query.filter_by(sno=sno).first()
+    todo = Todo.query.get(sno)
     return render_template('update.html', todo=todo)
 
 
 @app.route("/delete/<int:sno>")
 def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
+    todo = Todo.query.get(sno)
     db.session.delete(todo)
     db.session.commit()
 
